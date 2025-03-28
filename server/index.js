5a3e39338527998c6494b4db3c5aa42b8e45f4e9
Atul Chakrawarty <atul.chakrawarty@gmail.com>
2025-03-28 23:28:25 +0530

    1 Added user Routes and User Model. 2 Added Registration and checks against double registration

--- server/index.js
+++ server/index.js
@@ -1,15 +1,26 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 const PORT = 5000;
+const userRoutes = require("./routes/userRoute");
 
 mongoose
-  .connect("mongodb+srv://atulchakrawarty:gLAaQiH7afazN90l@lldcluster0.vbefk8i.mongodb.net/BMS?retryWrites=true&w=majority&appName=LLDCluster0")
+  .connect(
+    "mongodb+srv://atulchakrawarty:gLAaQiH7afazN90l@lldcluster0.vbefk8i.mongodb.net/BMS?retryWrites=true&w=majority&appName=LLDCluster0"
+  )
   .then(() => console.log("DB connected"))
   .catch((err) => {
     console.log(err);
   });
 
+
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.use("/api/users", userRoutes);
+
 app.listen(PORT, () => {
-    console.log(`Server started and is running on port ${PORT}`);
+  console.log(`Server started and is running on port ${PORT}`);
 });
