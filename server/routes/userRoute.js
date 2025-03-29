0672720c8497b26665639a159158fdc6b8e91e07
Atul Chakrawarty <atul.chakrawarty@gmail.com>
2025-03-29 18:31:38 +0530

    1 Added jwt token and linked with user login to be stored in localstorage. 2. Installed dotenv and setup secret key

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // Route for Register:
 
@@ -12,65 +13,64 @@ router.post("/register", async (req, res) => {
     if (userExists) {
       res.send({
         success: false,
-        message: "user already Exists",
+        message: "user already exists",
       });
     }
 
-     // Hashing The password
+    // Hashing The password
 
-     const salt = await bcrypt.genSalt(10);
+    const salt = await bcrypt.genSalt(10);
 
-     const hashedPassword = await bcrypt.hash(req.body.password, salt);
-     req.body.password = hashedPassword;
- 
-     console.log(salt);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    req.body.password = hashedPassword;
+
+    console.log(salt);
 
     const newUser = await User(req.body);
-  await newUser.save();
+    await newUser.save();
 
-  res.send({
-    success: true,
-    message: "User registered successfully",
-  });
-
+    res.send({
+      success: true,
+      message: "User registered successfully",
+    });
   } catch (error) {
     console.log(error);
   }
-
-  
 });
 
 router.post("/login", async (req, res) => {
-    try {
-      const user = await User.findOne({ email: req.body.email });
-  
-      console.log(user);
-  
-      if (!user) {
-        return res.send({
-          success: false,
-          message: "You are not registered! Please register first",
-        });
-      }
-      const validPassword = await bcrypt.compare(
-        req.body.password,
-        user.password
-      );
-      if (!validPassword) {
-        return res.send({
-          success: false,
-          message: "Sorry, invalid password entered!",
-        });
-      }
-    
-      res.send({
-        success: true,
-        message: "User Logged in",
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    console.log(user);
+
+    if (!user) {
+      return res.send({
+        success: false,
+        message: "You are not registered! Please register first",
       });
-    } catch (error) {
-      console.log(err);
     }
-  });
-  
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword) {
+      return res.send({
+        success: false,
+        message: "Sorry, invalid password entered!",
+      });
+    }
+
+    const token = jwt.sign({userId : user._id} , `${process.env.SECRET_KEY}` , {expiresIn:  "1d"})
+
+    res.send({
+      success: true,
+      message: "User Logged in",
+      token : token
+    });
+  } catch (error) {
+    console.log(err);
+  }
+});
 
 module.exports = router;
